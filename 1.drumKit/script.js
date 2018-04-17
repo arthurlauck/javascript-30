47f286c535ecf33ea44ac5393f84dacce964a6d8
Arthur Nascimento <arthur@dooca.com.br>
2018-04-17 13:37:45 -0300

    day 1

--- 1.drumKit/script.js
+++ 1.drumKit/script.js
@@ -1,3 +1,23 @@
 window.addEventListener('keydown', function(e) {
-	console.log(e);
+	var sound = document.querySelector(`source[data-key="${e.keyCode}"]`);
+	var key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
+	// If not found the audio or key
+	if(!sound || !key) return;
+
+	// Play sound
+	sound = new Audio(sound.src);
+	sound.play();
+	
+	// Add class
+	key.classList.add("playing");
+});
+
+// Select all keys
+var keys = document.querySelectorAll(".keys");
+// Event transition end
+keys.forEach(element => {
+	element.addEventListener('transitionend', (e) => {
+		e.target.classList.remove("playing");
+	});
 });
