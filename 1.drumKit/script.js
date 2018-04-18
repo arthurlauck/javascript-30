b95d47b1227dece3940a6b4cb10f113a2deb3361
Arthur Nascimento <arthur@dooca.com.br>
2018-04-18 12:23:40 -0300

    final drumkit

--- 1.drumKit/script.js
+++ 1.drumKit/script.js
@@ -1,15 +1,20 @@
+function removeTransition(e) {
+	if (e.propertyName !== 'transform') return;
+	e.target.classList.remove('playing');
+}
+
 window.addEventListener('keydown', (e) => {
 	// Select elements
-	var sound = document.querySelector(`source[data-key="${e.keyCode}"]`);
+	var sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	var key = document.querySelector(`div[data-key="${e.keyCode}"]`);
 
 	// If not found the audio or key
 	if(!sound || !key) return;
 
 	// Play sound
-	sound = new Audio(sound.src);
+	// sound = new Audio(sound.src); // doesnt need to create a new Audio element
+	sound.currentTime = 0;
 	sound.play();
-	
 	// Add class when playing
 	key.classList.add("playing");
 });
@@ -19,7 +24,5 @@ var keys = document.querySelectorAll(".keys");
 
 // Event transition end
 keys.forEach((ele) => {
-	ele.addEventListener('transitionend', (e) => {
-		e.target.classList.remove("playing");
-	});
+	ele.addEventListener('transitionend', removeTransition);
 });
